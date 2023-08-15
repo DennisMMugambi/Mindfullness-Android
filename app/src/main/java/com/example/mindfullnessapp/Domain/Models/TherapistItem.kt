2d92a4e45f7edeee09ce58cdbc2cfd1de69d3ba4
Dennis Mugambi <dennis.mutwiri.mugambi@gmail.com>
2023-08-15 15:37:14 +0300

    Complete home fragment design

---
+++ app/src/main/java/com/example/mindfullnessapp/Domain/Models/TherapistItem.kt
@@ -0,0 +1,12 @@
+package com.example.mindfullnessapp.Domain.Models
+
+data class TherapistItem(
+    val image : Int,
+    val name : String,
+    val rating : String,
+    val education : String,
+    val experience : String,
+    val price : String,
+    val oldprice : String,
+    val availability : String
+)
--- app/src/main/java/com/example/mindfullnessapp/Home/HomeFragment.kt
+++ app/src/main/java/com/example/mindfullnessapp/Home/HomeFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mindfullnessapp.Adapters.GenericAdapter
 import com.example.mindfullnessapp.Adapters.MeditationsAdapter
 import com.example.mindfullnessapp.Domain.Models.MeditationItem
+import com.example.mindfullnessapp.Domain.Models.TherapistItem
 import com.example.mindfullnessapp.R
 import com.example.mindfullnessapp.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -52,6 +56,68 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         val layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
         horizontalRecyclerView.layoutManager = layoutManager
         horizontalRecyclerView.adapter = adapter
+
+        showTherapists()
+    }
+
+    private fun showTherapists() {
+        // Suppose you have a list of items with different types
+       val therapistList: List<TherapistItem> = listOf(
+            TherapistItem(
+                image = R.drawable.feeling_awful,
+                name = "Dr. Panoor Doe",
+                rating = "4.8",
+                education = "PhD in Psychology",
+                experience = "10+ years of experience",
+                price = "KSHS 5000",
+                oldprice = "KSHS 7000",
+                availability = "9AM to 1AM"
+            ),
+            TherapistItem(
+                image = R.drawable.feeling_awful,
+                name = "Dr Janic Doe",
+                rating = "4.1",
+                education = "MA in Counseling",
+                experience = "8+ years of experience",
+                price = "KSHS 4000",
+                oldprice = "KSHS 4500",
+                availability = "2PM to 5PM"
+            ),
+           TherapistItem(
+               image = R.drawable.feeling_awful,
+               name = "Dr Kevin Doe",
+               rating = "4.7",
+               education = "MA in Psychiatry",
+               experience = "12+ years of experience",
+               price = "KSHS 6000",
+               oldprice = "KSHS 7500",
+               availability = "9AM to 5PM"
+           ),
+            // Add more therapists here...
+        )
+
+        // Create an instance of the GenericAdapter
+        val adapter = GenericAdapter(therapistList, R.layout.therapist_card) { itemView, item ->
+            // Bind your data to the itemView
+            // You can access the views in itemView using findViewById or ViewBinding
+            itemView.findViewById<ImageView>(R.id.therapist_image).setImageResource(item.image)
+            itemView.findViewById<TextView>(R.id.therapist_title).text = item.name
+            itemView.findViewById<TextView>(R.id.therapist_rating).text = item.rating
+            itemView.findViewById<TextView>(R.id.therapist_education).text = item.education
+            itemView.findViewById<TextView>(R.id.therapist_experience).text = item.experience
+            itemView.findViewById<TextView>(R.id.therapist_price).text = item.price
+            itemView.findViewById<TextView>(R.id.therapist__old_price).text = item.oldprice
+            itemView.findViewById<TextView>(R.id.therapist_availability_time).text = item.availability
+        }
+
+        // Set the adapter to your RecyclerView
+        val layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        binding.therapistsRV.layoutManager = layoutManager
+        binding.therapistsRV.adapter = adapter
+
+    }
+
+    private fun hideBottomNavOnScroll() {
 
     }
 
